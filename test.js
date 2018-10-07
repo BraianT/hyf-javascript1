578d99b7499f580f22e544ee2b2cb0d39ec2101c
BraianT <42869449+BraianT@users.noreply.github.com>
2018-10-07 04:36:38 -0400

    js
    first

---
+++ test.js
@@ -0,0 +1,12 @@
+console.log("Hello World!");//1
+
+console.log("I'm awesome"); //2. quotation marks
+
+var x; //3.1
+console.log("The vslue of x will be: String Test");//3.2
+
+console.log(x); //3.3
+
+x = "String Test";
+console.log(x);
+
